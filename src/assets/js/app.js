e63c262a058aca6076fcdb8a864042d4d893285c
Doug Richar <drichar@gmail.com>
2018-09-26 01:37:41 -0400

    Header & footer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,24 @@
 import $ from 'jquery'
 window.$ = $
 import 'bootstrap'
+
+
+const header = document.querySelector('.sd-header');
+
+function debounce(func, wait) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+function onScroll() {
+  let currScrollTop = window.scrollY;
+  let isScrolling = currScrollTop > 0;
+
+  header.classList.toggle('sd-header-scroll', isScrolling);
+}
+
+window.addEventListener('scroll', debounce(onScroll, 16));
