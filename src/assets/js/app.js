83c650710945a32b925e233283dc9e9379a2c0a4
Doug Richar <drichar@gmail.com>
2018-09-30 06:14:17 -0400

    Order Now button scoll & form focus

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,33 +6,47 @@ import 'bootstrap'
 /**
  * Scrolling menu behavior
  */
-const header = document.querySelector('.sd-header')
-const hero = document.querySelector('.sd-hero')
+const header = document.querySelector('.sd-header');
+const hero = document.querySelector('.sd-hero');
 
 function debounce(fn, wait) {
-  let timeout
+  let timeout;
   return function(...args) {
-    const context = this
-    clearTimeout(timeout)
-    timeout = setTimeout(() => fn.apply(context, args), wait)
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn.apply(context, args), wait);
   };
 }
 
 function onScroll() {
-  let currScrollTop = window.scrollY
-  let isScrollingSm = currScrollTop > 0
-  let isScrollingLg = currScrollTop > hero.offsetHeight
+  let currScrollTop = window.scrollY;
+  let isScrollingSm = currScrollTop > 0;
+  let isScrollingLg = currScrollTop > hero.offsetHeight;
 
-  header.classList.toggle('sd-header-scroll-sm', isScrollingSm)
-  header.classList.toggle('sd-header-scroll-lg', isScrollingLg)
+  header.classList.toggle('sd-header-scroll-sm', isScrollingSm);
+  header.classList.toggle('sd-header-scroll-lg', isScrollingLg);
 }
 
-window.addEventListener('scroll', debounce(onScroll, 16))
+window.addEventListener('scroll', debounce(onScroll, 16));
+
+
+/**
+ * Jump to form on Order Now click
+ */
+(function() {
+  $('.sd-btn-order').click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+    $('#sdFullName').focus();
+  });
+})();
 
 
 /**
  * Stop video playback when modal is closed
  */
-$('.modal').on('hidden.bs.modal', function (e) {
-  $('.modal iframe').attr('src', $('.modal iframe').attr('src'));
-});
+(function() {
+  $('.modal').on('hidden.bs.modal', function (e) {
+    $('.modal iframe').attr('src', $('.modal iframe').attr('src'));
+  });
+})();
